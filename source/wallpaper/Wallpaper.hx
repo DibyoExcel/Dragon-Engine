066369de1962359c8dbde809362f8c722a949793
DubEnderDragon <dibyo.excel.90@gmail.com>
2025-11-08 13:10:38 +0700

    fix small

--- source/wallpaper/Wallpaper.hx
+++ source/wallpaper/Wallpaper.hx
@@ -11,11 +11,11 @@ class Wallpaper {
     /**
      * Changes wallpaper which is in `path`. Do a warning for players in your mod if you want use this function!!!!
      * @param path Path to image
-     * @param absolute If false, `path` => mods/images/`path`.png
+     * @param absolute If false, `path` => it set to yourappfolder/
      * @return Bool - If false, wallpaper wasnt changed / target isnt Windows
      */
     public static function changeWallpaper(path:String, ?absolute:Bool = false):Bool {
-        var fullPath = absolute ? path : FileSystem.absolutePath('mods/images/' + path + '.png');
+        var fullPath = absolute ? path : FileSystem.absolutePath(path);
         return #if windows untyped __cpp__('SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, (void*){0}.c_str(), SPIF_UPDATEINIFILE)', fullPath) #else false #end;
     }
 }
